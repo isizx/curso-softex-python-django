98af086e2b244f3cc99c106f838ff8c9df9f5531
IsisFerreira <99364477+IsisFerreira@users.noreply.github.com>
2025-09-06 11:51:48 -0300

    exercício fibonacci

---
+++ modulo1/aula6/ex1.py
@@ -0,0 +1,23 @@
+''' Exercício 17: Sequência de Fibonacci
+● Peça um número n ao usuário.
+● Use um while para gerar e imprimir os primeiros n termos da sequência de Fibonacci (0,
+1, 1, 2, 3, 5, ...).'''
+
+n = int(input("\nDigite o número de termos da sequência de Fibonacci que você deseja gerar: \n"))
+
+t1, t2 = 0, 1
+contador = 0
+
+if n <= 0:
+   print("Por favor, digite um número inteiro positivo.")
+elif n == 1:
+   print(f"A sequência de Fibonacci com {n} termo é:")
+   print(t1)
+else:
+   print(f"Os primeiros {n} termos da sequência de Fibonacci são:")
+   while contador < n:
+       print(f'termo n°{contador+1}: {t1}\n')
+       proximo_termo = t1 + t2
+       t1 = t2
+       t2 = proximo_termo
+       contador += 1
